37e8bf9cb20626b4d71309b65c2b5c60feb1b76c
natanaelcloud <natanpaivacosta@gmail.com>
2024-09-27 17:38:16 -0300

    alterar name de let e adicionar console

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
 
-let value = prompt('Insira o valor do produto: ')
+let productValue = Number(prompt('Insira o valor do produto: '))
 //O código let value = prompt('Insira o valor do produto: ') exibe uma caixa de diálogo para o usuário inserir um valor. O que o usuário digitar será armazenado como uma string na variável value//
+
+console.log(typeof(productValue))
