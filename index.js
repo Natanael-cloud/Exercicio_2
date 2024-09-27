f64f3ab7203739293cde5b788d5762c5e90eb61d
natanaelcloud <natanpaivacosta@gmail.com>
2024-09-27 17:09:40 -0300

    criar primeira let de nome value

---
+++ index.js
@@ -0,0 +1,4 @@
+
+
+let value = prompt('Insira o valor do produto: ')
+//O código let value = prompt('Insira o valor do produto: ') exibe uma caixa de diálogo para o usuário inserir um valor. O que o usuário digitar será armazenado como uma string na variável value//
