dfe88866287a28d7a41bfb2f2a5c9a2b568bf294
natanaelcloud <natanpaivacosta@gmail.com>
2024-09-27 18:01:27 -0300

    adicionar comentários explicando o IF e Else

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ if (productValue >=20){
     document.getElementById('resultado').innerHTML = 'Aprovado'
 
 }
+//na linha 6: Condição 'If', se o valor do produto(productValue) for maior(>) ou igual(=) a 20
+//na linha 7: irá imprimir o resultado "Aprovado"
 else{
      document.getElementById('resultado').innerHTML = 'Reprovado'
-
+  //caso o valor não esteja dentro dos paramentros do If da linha 6, irá imprimir o resultado "Reprovado"
 }
